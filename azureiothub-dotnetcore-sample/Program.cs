fba44e225ca3f879dddef960f2a0e7708b263f40
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-05-29 14:03:27 +0200

    Add project files.

---
+++ azureiothub-dotnetcore-sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace azureiothub_dotnetcore_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
